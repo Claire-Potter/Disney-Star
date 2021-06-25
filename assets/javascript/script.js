450df7628e7687a124427b23a8bb705598b6de5b
Claire Potter <clairepotter019@gmail.com>
2021-06-25 15:08:21 +0000

    Add DOMContent EventListener and create constants for buttons. Update html to align with functions and buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,12 @@
+// Wait for the DOM to finish loading before running any of the functions on the page
+document.addEventListener("DOMContentLoaded");
+
+// Create constants for button event listeners
+const generateButton = document.getElementById('generateCharacter');
+const nextButton = document.getElementById('nextButton');
+const prevButton = document.getElementById('prevButton');
+
+
 function generateCharacter() {
 
 }
