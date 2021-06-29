3af1922dc28359466a11dbf125c52dea17bd31c0
Claire Potter <clairepotter019@gmail.com>
2021-06-29 17:42:08 +0000

    Add answer options and right or wrong indicators

--- assets/javascript/quizpage.js
+++ assets/javascript/quizpage.js
@@ -37,7 +37,7 @@ const answersIndicatorContainer = document.querySelector(".answers-indicator");
 const homeBox = document.querySelector(".home-box");
 const quizBox = document.querySelector(".quiz-box");
 const resultBox = document.querySelector(".result-box");
-const questionLimit = 5; // if you want all questions "quiz.length"
+const questionLimit = 25; // if you want all questions "quiz.length"
 
 let questionCounter = 0;
 let currentQuestion;
@@ -55,7 +55,7 @@ const quiz = [{
         options: ['1951', '1957', '1953', '1955', ],
         optionsImages: ['disneyLand1.jpg', 'disneyLand2.jpg', 'disneyLand3.jpg', 'disneyLand4.jpg', ],
         imageTitles: ['The opening day at Disneyland', 'Mickey and Minnie Mouse with two children', 'Walt Disney at Disneyland', 'A Disneyland Parade', ],
-        answer: '3',
+        answer: 3,
     },
     {
         q: 'Who was the first cartoon character to get a star on the Hollywood Walk of Fame?​',
@@ -63,7 +63,7 @@ const quiz = [{
         options: ['Snow White', 'Mickey Mouse', 'Donald Duck', 'Cinderella', ],
         optionsImages: ['snowWhite.png', 'mickeyMouse.png', 'donaldDuck.png', 'cinderellaPoor.png', ],
         imageTitles: ['Snow White', 'Mickey Mouse', 'Donald Duck', 'Cinderella', ],
-        answer: '1',
+        answer: 1,
     },
     {
         q: 'How many brothers does Prince Hans of the Southern Isles have in Frozen?​​',
@@ -71,7 +71,7 @@ const quiz = [{
         options: ['12', '7', '15', '5', ],
         optionsImages: ['hansAnna.png', 'hansElsa.png', 'hansKristoff.png', 'hansAnnaPunch.png', ],
         imageTitles: ['Hans with Anna', 'Hans with Elsa', 'Hans with Kristoff', 'Anna punches Hans', ],
-        answer: '0',
+        answer: 0,
     },
     {
         q: 'What was the name of Walt Disney’s first cartoon character?​​',
@@ -79,7 +79,7 @@ const quiz = [{
         options: ['Ralph', 'Donald Duck', 'Oswald the Lucky Rabbit', 'Mickey Mouse', ],
         optionsImages: ['ralph.png', 'mickeyMouse.png', 'oswaldTheLuckyRabbit.png', 'donaldDuck.png', ],
         imageTitles: ['Ralph', 'Donald Duck', 'Oswald the Lucky Rabbit', 'Mickey Mouse', ],
-        answer: '2',
+        answer: 2,
     },
     {
         q: 'Who trained Hercules to be a hero?',
@@ -87,7 +87,7 @@ const quiz = [{
         options: ['Pygmalion', 'Philoctetes', 'Prometheus', 'Parentheses', ],
         optionsImages: ['hercules1.png', 'hercules2.png', 'hercules3.png', 'hercules4.png', ],
         imageTitles: ['A short goat man', 'A short goat man eating grapes', 'Hercules, Pegasus and the goat man', 'Hercules shooting the goat man', ],
-        answer: '1',
+        answer: 1,
     },
     {
         q: 'What is the name of Ariel and Prince Eric’s daughter?​',
@@ -95,7 +95,7 @@ const quiz = [{
         options: ['Marina', 'Alanna', 'Oceana', 'Melody', ],
         optionsImages: ['daughterFriends.png', 'arielDaughter.png', 'ericDaughter.png', 'arielAndHerDaughter.png', ],
         imageTitles: ['Girl with a penguin and a walrus', 'Ariel and Eric Daughter', 'Eric and Daughter', 'Ariel and her daughter'],
-        answer: '3',
+        answer: 3,
     },
     {
         q: 'What is the name of the castle in Shanghai Disneyland Park?​',
@@ -103,7 +103,7 @@ const quiz = [{
         options: ['Cinderella Dream Castle', 'Atlantis', 'The Enchanted Storybook Castle', 'Shanghai Palace of Mystery', ],
         optionsImages: ['castleShanghai1.jpg', 'castleShanghai2.jpg', 'castleShanghai3.jpg', 'castleShanghai4.jpg', ],
         imageTitles: ['The castle at Disneyland Shanghai', 'The castle at Disneyland Shanghai', 'The castle at Disneyland Shanghai', 'The castle at Disneyland Shanghai', ],
-        answer: '2',
+        answer: 2,
     },
     {
         q: 'Which Disney Princess sings “Once Upon a Dream”?​​',
@@ -111,7 +111,7 @@ const quiz = [{
         options: ['Cinderella', 'Snow White', 'Belle, Beauty and the Beast', 'Aurora, Sleeping Beauty', ],
         optionsImages: ['cinderella.png', 'snowWhite.png', 'belle.png', 'aurora.png', ],
         imageTitles: ['Cinderella', 'Snow White', 'Belle, Beauty and the Beast', 'Aurora, Sleeping Beauty', ],
-        answer: '3',
+        answer: 3,
     },
     {
         q: 'Emperor Kuzco turns into what animal in The Emperor’s New Groove?',
@@ -119,7 +119,7 @@ const quiz = [{
         options: ['Llama', 'Camel', 'Alpaca', 'Vicunas', ],
         optionsImages: ['kuzco1.png', 'kuzco2.png', 'kuzco3.png', 'kuzco4.png', ],
         imageTitles: ['Emperor Kuzco', 'Animal Kuzco', 'Kuzco and friend', 'Animal Kuzco', ],
-        answer: '0',
+        answer: 0,
     },
     {
         q: 'Which of these frisky felines is the beloved companion of Alice in Wonderland?',
@@ -127,7 +127,7 @@ const quiz = [{
         options: ['Tibs', 'Dinah', 'Marie', 'Toulouse', ],
         optionsImages: ['tibbs.png', 'dinah.png', 'marie.png', 'toulouse.png', ],
         imageTitles: ['Tibs', 'Dinah', 'Marie', 'Toulouse', ],
-        answer: '1',
+        answer: 1,
     },
     {
         q: 'Disney has recently made many live action versions of our favourite Disney Movies. Which of these couples do not feature in a live action film?',
@@ -135,7 +135,7 @@ const quiz = [{
         options: ['Cinderella and Prince Charming', 'Belle and Beast', 'Princess Anna and Prince Hans', 'Aladdin and Princess Jasmine', ],
         optionsImages: ['cinderellaAndPrinceDancing.png', 'belleAndBeast.png', 'annaHansDance.png', 'aladdinAndJasmine.png', ],
         imageTitles: ['Cinderella and Prince Charming', 'Belle and Beast', 'Princess Anna and Prince Hans', 'Aladdin and Princess Jasmine', ],
-        answer: '2',
+        answer: 2,
     },
     {
         q: 'This super hero is set to feature in a new full length feature coming out July 2021, select the correct hero:',
@@ -143,7 +143,7 @@ const quiz = [{
         options: ['Captain America', 'Captain Marvel', 'Black Widow', 'Thor', ],
         optionsImages: ['captainAmerica.png', 'captainMarvel.png', 'blackWidow.png', 'thor.png', ],
         imageTitles: ['Captain America', 'Captain Marvel', 'Black Widow', 'Thor', ],
-        answer: '2',
+        answer: 2,
     },
     {
         q: 'The late Robin Williams voiced this beloved character who brought much comic relief to the film:',
@@ -151,7 +151,7 @@ const quiz = [{
         options: ['Baymax', 'Lightning McQueen', 'Genie', 'Mad Hatter', ],
         optionsImages: ['baymax.png', 'lightningMcqueen.png', 'genie.png', 'madHatter.png', ],
         imageTitles: ['Baymax', 'Lightning McQueen', 'Genie', 'Mad Hatter', ],
-        answer: '0',
+        answer: 2,
     },
     {
         q: 'This little fellow was separated from his mama at the beginning of the movie, luckily they were reunited:',
@@ -159,7 +159,7 @@ const quiz = [{
         options: ['Dumbo', 'Thumper', 'Chip', 'Bambi', ],
         optionsImages: ['dumboFlying.png', 'thumper.png', 'chip.png', 'bambi.png', ],
         imageTitles: ['Dumbo', 'Thumper', 'Chip', 'Bambi', ],
-        answer: '0',
+        answer: 0,
     },
     {
         q: 'Which of these characters made quite a spectacle when he thought the sky was falling?',
@@ -167,7 +167,7 @@ const quiz = [{
         options: ['Dormouse', 'Hulk', 'Flik', 'Chicken Little', ],
         optionsImages: ['dormouse.png', 'hulk.png', 'flik.png', 'chickenLittle.jpg', ],
         imageTitles: ['Dormouse', 'Hulk', 'Flik', 'Chicken Little', ],
-        answer: '3',
+        answer: 3,
     },
     {
         q: 'Which of these lovely ladies is not an official Disney Princess?',
@@ -175,7 +175,7 @@ const quiz = [{
         options: ['Merida', 'Alice', 'Cinderella', 'Elsa', ],
         optionsImages: ['merida.png', 'aliceInWonderland.png', 'cinderella.png', 'elsa.png', ],
         imageTitles: ['Merida', 'Alice', 'Cinderella', 'Elsa', ],
-        answer: '1',
+        answer: 1,
     },
     {
         q: 'This friendly canine is the father of not 1 but 101 puppies:',
@@ -183,7 +183,7 @@ const quiz = [{
         options: ['Colonel', 'Perdita', 'Clip', 'Pongo', ],
         optionsImages: ['colonel.png', 'perdita.png', 'dodger.png', 'pongo.png', ],
         imageTitles: ['Colonel', 'Perdita', 'Clip', 'Pongo', ],
-        answer: '3',
+        answer: 3,
     },
     {
         q: 'Name the film and the character heard in the following sound clip:',
@@ -191,7 +191,7 @@ const quiz = [{
         options: ['Monsters, Inc - Sulley', "A Bug's Life - Flik", 'Good Dinosaur - Arlo', 'Onward - Ian', ],
         optionsImages: ['sulley.png', 'flik.png', 'arlo.png', 'ian.png', ],
         imageTitles: ['Monsters, Inc - Sulley', "A Bug's Life - Flik", 'Good Dinosaur - Arlo', 'Onward - Ian', ],
-        answer: '1',
+        answer: 1,
     },
     {
         q: 'Name the film and the character heard in the following sound clip:',
@@ -199,7 +199,7 @@ const quiz = [{
         options: ['Sleeping Beauty - Aurora', 'Brave - Merida', 'The Princess and the Frog - Tiana', 'Beauty and the Beast - Belle', ],
         optionsImages: ['aurora.png', 'merida.png', 'tiana.png', 'belle.png', ],
         imageTitles: ['Sleeping Beauty - Aurora', 'Brave - Merida', 'The Princess and the Frog - Tiana', 'Beauty and the Beast - Belle', ],
-        answer: '3',
+        answer: 3,
     },
     {
         q: 'Name the character heard in the following sound clip:',
@@ -207,7 +207,7 @@ const quiz = [{
         options: ['Donald Duck', 'Goofy', 'Mickey Mouse', 'Scrooge McDuck', ],
         optionsImages: ['donaldDuck.png', 'goofy.png', 'mickeyMouse.png', 'pete.png', ],
         imageTitles: ['Donald Duck', 'Goofy', 'Mickey Mouse', 'Pete', ],
-        answer: '0',
+        answer: 0,
     },
     {
         q: 'Name the film and the character heard in the following sound clip:',
@@ -215,7 +215,7 @@ const quiz = [{
         options: ['Finding Dory - Merlin', 'Finding Nemo - Merlin', 'Shark Tale - Oscar', 'Shark Bait - Pisces', ],
         optionsImages: ['merlinDory.png', 'merlin.png', 'sharkTaleOscar.png', 'sharkBaitPi.png', ],
         imageTitles: ['Finding Dory - Merlin', 'Finding Nemo - Merlin', 'Shark Tale - Oscar', 'Shark Bait - Pisces', ],
-        answer: '1',
+        answer: 1,
     },
     {
         q: 'Name the film and the character heard in the following sound clip:',
@@ -223,7 +223,7 @@ const quiz = [{
         options: ['Tangled - Rapunzel', 'The Little Mermaid - Ariel', 'Beauty and the Beast - Belle', 'Frozen- Elsa', ],
         optionsImages: ['rapunzel.png', 'ariel.png', 'belle.png', 'elsa.png', ],
         imageTitles: ['Tangled - Rapunzel', 'The Little Mermaid - Ariel', 'Beauty and the Beast - Belle', 'Frozen- Elsa', ],
-        answer: '3',
+        answer: 3,
     },
     {
         q: 'Name the character heard in the following sound clip:',
@@ -231,7 +231,7 @@ const quiz = [{
         options: ['Hawkeye aka Clint Barton', 'The Hulk aka Bruce Banner', 'Black Panther aka King TChalla', 'Iron Man aka Tony Stark', ],
         optionsImages: ['hawkeye.png', 'hulk.png', 'blackPanther.png', 'ironMan.png', ],
         imageTitles: ['Hawkeye aka Clint Barton', 'The Hulk aka Bruce Banner', 'Black Panther aka King TChalla', 'Iron Man aka Tony Stark', ],
-        answer: '3',
+        answer: 3,
     },
     {
         q: 'Name the character heard in the following sound clip:',
@@ -239,7 +239,7 @@ const quiz = [{
         options: ['Mary Poppins', 'Nanny McPhee', 'Maria', 'Mother Carey', ],
         optionsImages: ['maryPoppins.jpg', 'nannyMcphee.png', 'maria.png', 'motherCarey.png', ],
         imageTitles: ['Mary Poppins', 'Nanny McPhee', 'Maria', 'Mother Carey', ],
-        answer: '0',
+        answer: 0,
     },
     {
         q: 'Name the film and the character heard in the following sound clip:',
@@ -247,7 +247,7 @@ const quiz = [{
         options: ['Star Wars: Attack of the Clones - Anakin Skywalker', 'Star Wars: The Empire Strikes Back - Darth Vader', 'Star Wars: Revenge of the Sith - Anakin Skywalker', 'Star Wars: A New Hope - Darth Vader', ],
         optionsImages: ['anakin.png', 'darthVader.png', 'anakin2.png', 'darthVader2.png', ],
         imageTitles: ['Star Wars: Attack of the Clones - Anakin Skywalker', 'Star Wars: The Empire Strikes Back - Darth Vader', 'Star Wars: Revenge of the Sith - Anakin Skywalker', 'Star Wars: A New Hope - Darth Vader', ],
-        answer: '1',
+        answer: 1,
     },
 ]
 
@@ -293,7 +293,6 @@ function getNewQuestion() {
        availableOptions.push(i)
     }
     optionContainer.innerHTML = '';
-    let animationDelay = 0.15;
     // create options in html
     for(let i=0; i<optionLen; i++){
        // random option
@@ -309,8 +308,8 @@ function getNewQuestion() {
        optionContainer.appendChild(option);
        option.setAttribute("onclick","getResult(this)");
     }
-   console.log(availableQuestions)
-   console.log(availableOptions)
+   //console.log(availableQuestions)
+  // console.log(availableOptions)
     questionCounter++;
  }
 /**
@@ -340,6 +339,38 @@ function play() {
     audio.play();
 };
 
+// get the result of current attempt question
+function getResult(element){
+    const id = parseInt(element.id);
+    // get the answer by comparing the id of clicked option
+    if(id === currentQuestion.answer){
+        // set the green color to the correct option
+        element.classList.add("correct");
+  
+    }
+    else{
+        // set the red color to the incorrect option
+        element.classList.add("wrong");
+    };
+
+     // if the answer is incorrect then show the correct option by adding green color the correct option
+       const optionLen = optionContainer.children.length;
+       for(let i=0; i<optionLen; i++){
+           if(parseInt(optionContainer.children[i].id) === currentQuestion.answer){
+             optionContainer.children[i].classList.add("correct");  		
+           } }  
+    unclickableOptions();
+  attempt++;
+        }
+
+// make all the options unclickable once the user select a option (RESTRICT THE USER TO CHANGE THE OPTION AGAIN)
+function unclickableOptions(){
+    const optionLen = optionContainer.children.length;
+    for(let i=0 ; i<optionLen; i++){
+    	optionContainer.children[i].classList.add("already-answered");
+    }
+ }
+    
 //add event listener to submit button to trigger next question
 
 submitButton.addEventListener('click', nextQuestion);
@@ -349,13 +380,12 @@ submitButton.addEventListener('click', nextQuestion);
  * It also ends the question if the final question has been reached and calls the results page.
  */
 
-function nextQuestion() {
-    if (questionCounter === quiz.length) {
-        console.log('quiz over');
-    } else {
-        getNewQuestion()
+ function nextQuestion(){
+    if(questionCounter === questionLimit){
+          quizOver();
     }
-};
-
-
+    else{
+         getNewQuestion();
+    }
+  }
 
