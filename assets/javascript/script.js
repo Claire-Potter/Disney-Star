ca89156173de548afeb41b5a536c03f407ea2fe0
Claire Potter <clairepotter019@gmail.com>
2021-07-07 11:56:08 +0000

    Format and check each file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,385 +16,385 @@ let selectedCharacter = 0;
  */
 
 const characters = [{
-      character: 'Elena',
-      characterImageArea: 'elena.PNG',
-
-   },
-   {
-      character: 'Pluto',
-      characterImageArea: 'pluto.PNG',
-
-   },
-   {
-      character: 'Grumpy',
-      characterImageArea: 'grumpy.PNG',
-
-   },
-   {
-      character: 'Moana',
-      characterImageArea: 'moana.PNG',
-
-   },
-   {
-      character: 'Olaf',
-      characterImageArea: 'olaf.PNG',
-
-   },
-   {
-      character: 'Bo Peep',
-      characterImageArea: 'boPeep.PNG',
-
-   },
-   {
-      character: 'Lightening McQueen',
-      characterImageArea: 'lighteningMcQueen.PNG',
-
-   },
-   {
-      character: 'Simba',
-      characterImageArea: 'simba.PNG',
-
-   },
-   {
-      character: 'Yoda',
-      characterImageArea: 'yoda.PNG',
-
-   },
-   {
-      character: 'Ant Man',
-      characterImageArea: 'antMan.PNG',
-
-   },
-   {
-      character: 'Jack Skellington',
-      characterImageArea: 'jackSkellington.PNG',
-
-   },
-   {
-      character: 'Ariel',
-      characterImageArea: 'ariel.PNG',
-
-   },
-   {
-      character: 'Mike Wazowski',
-      characterImageArea: 'mikeWazowski.PNG',
-
-   },
-   {
-      character: 'Marie',
-      characterImageArea: 'marie.PNG',
-
-   },
-   {
-      character: 'Venom',
-      characterImageArea: 'venom.PNG',
-
-   },
-   {
-      character: 'Goofy',
-      characterImageArea: 'goofy.PNG',
-
-   },
-   {
-      character: 'Black Widow',
-      characterImageArea: 'blackWidow.PNG',
-
-   },
-   {
-      character: 'Alice',
-      characterImageArea: 'alice.PNG',
-
-   },
-   {
-      character: 'Mickey Mouse',
-      characterImageArea: 'mickeyMouse.PNG',
-
-   },
-   {
-      character: 'Hulk',
-      characterImageArea: 'hulk.PNG',
-
-   },
-   {
-      character: 'Mary Poppins',
-      characterImageArea: 'maryPoppins.PNG',
-
-   },
-   {
-      character: 'Woody',
-      characterImageArea: 'woody.PNG',
-
-   },
-   {
-      character: 'Tinker-bell',
-      characterImageArea: 'tinkerBell.PNG',
-
-   },
-   {
-      character: 'Captain Marvel',
-      characterImageArea: 'captainMarvel.PNG',
-
-   },
-   {
-      character: 'Anna',
-      characterImageArea: 'anna.PNG',
-
-   },
-   {
-      character: 'Daisy Duck',
-      characterImageArea: 'daisyDuck.PNG',
-
-   },
-   {
-      character: 'R2 D2',
-      characterImageArea: 'r2D2.PNG',
-
-   },
-   {
-      character: 'Sulley',
-      characterImageArea: 'sulley.PNG',
-
-   },
-   {
-      character: 'Forky',
-      characterImageArea: 'forky.PNG',
-
-   },
-   {
-      character: 'Mulan',
-      characterImageArea: 'mulan.PNG',
-
-   },
-   {
-      character: 'Wall-E',
-      characterImageArea: 'wallE.PNG',
-
-   },
-   {
-      character: 'Cheshire Cat',
-      characterImageArea: 'cheshireCat.PNG',
-
-   },
-   {
-      character: 'Darth Vader',
-      characterImageArea: 'darthVader.PNG',
-
-   },
-   {
-      character: 'Pinocchio',
-      characterImageArea: 'pinocchio.PNG',
-
-   },
-   {
-      character: 'Spider Man',
-      characterImageArea: 'spiderMan.PNG',
-
-   },
-   {
-      character: 'Hercules',
-      characterImageArea: 'hercules.PNG',
-
-   },
-   {
-      character: 'Rex',
-      characterImageArea: 'rex.PNG',
-
-   },
-   {
-      character: 'Cinderella',
-      characterImageArea: 'cinderella.PNG',
-
-   },
-   {
-      character: 'Han Solo',
-      characterImageArea: 'hanSolo.PNG',
-
-   },
-   {
-      character: 'Captain America',
-      characterImageArea: 'captainAmerica.PNG',
-
-   },
-   {
-      character: 'Winnie the Pooh',
-      characterImageArea: 'winnieThePooh.PNG',
-
-   },
-   {
-      character: 'Chip',
-      characterImageArea: 'chip.PNG',
-
-   },
-   {
-      character: 'Elsa',
-      characterImageArea: 'elsa.PNG',
-
-   },
-   {
-      character: 'Piglet',
-      characterImageArea: 'piglet.PNG',
-
-   },
-   {
-      character: 'Thanos',
-      characterImageArea: 'thanos.PNG',
-
-   },
-   {
-      character: 'Dumbo',
-      characterImageArea: 'dumbo.PNG',
-
-   },
-   {
-      character: 'Stitch',
-      characterImageArea: 'stitch.PNG',
-
-   },
-   {
-      character: 'Rapunzel',
-      characterImageArea: 'rapunzel.PNG',
-
-   },
-   {
-      character: 'Lotso',
-      characterImageArea: 'lotso.PNG',
-
-   },
-   {
-      character: 'Doc McStuffins',
-      characterImageArea: 'docMcStuffins.PNG',
-
-   },
-   {
-      character: 'BB-8',
-      characterImageArea: 'bb8.PNG',
-
-   },
-   {
-      character: 'Tigger',
-      characterImageArea: 'tigger.PNG',
-
-   },
-   {
-      character: 'Belle',
-      characterImageArea: 'belle.PNG',
-
-   },
-   {
-      character: 'Aladdin',
-      characterImageArea: 'aladdin.png',
-
-   },
-   {
-      character: 'Black Panther',
-      characterImageArea: 'blackPanther.PNG',
-
-   },
-   {
-      character: 'Vampirina',
-      characterImageArea: 'vampirina.PNG',
-
-   },
-   {
-      character: 'Bambi',
-      characterImageArea: 'bambi.PNG',
-
-   },
-   {
-      character: 'Kermit',
-      characterImageArea: 'kermit.PNG',
-
-   },
-   {
-      character: 'Baymax',
-      characterImageArea: 'baymax.PNG',
-
-   },
-   {
-      character: 'Stormtrooper',
-      characterImageArea: 'stormtrooper.PNG',
-
-   },
-   {
-      character: 'Aurora',
-      characterImageArea: 'aurora.PNG',
-
-   },
-   {
-      character: 'Peter Pan',
-      characterImageArea: 'peterPan.PNG',
-
-   },
-   {
-      character: 'Iron Man',
-      characterImageArea: 'ironMan.PNG',
-
-   },
-   {
-      character: 'Jasmine',
-      characterImageArea: 'jasmine.PNG',
-
-   },
-   {
-      character: 'Grogu',
-      characterImageArea: 'grogu.PNG',
-
-   },
-   {
-      character: 'Tiana',
-      characterImageArea: 'tiana.PNG',
-
-   },
-   {
-      character: 'Thor',
-      characterImageArea: 'thor.PNG',
-
-   },
-   {
-      character: 'Buzz Lightyear',
-      characterImageArea: 'buzzLightyear.PNG',
-
-   },
-   {
-      character: 'Beast',
-      characterImageArea: 'beast.PNG',
-
-   },
-   {
-      character: 'Snow White',
-      characterImageArea: 'snowWhite.PNG',
-
-   },
-   {
-      character: 'Donald Duck',
-      characterImageArea: 'donaldDuck.PNG',
-
-   },
-   {
-      character: 'Merida',
-      characterImageArea: 'merida.PNG',
-
-   },
-   {
-      character: 'Eeyore',
-      characterImageArea: 'eeyore.PNG',
-
-   },
-   {
-      character: 'Jessie',
-      characterImageArea: 'jessie.PNG',
-
-   },
-   {
-      character: 'Pocahontas',
-      characterImageArea: 'pocahontas.PNG',
-
-   },
-   {
-      character: 'Minnie Mouse',
-      characterImageArea: 'minnieMouse.PNG',
-
-   },
+    character: 'Elena',
+    characterImageArea: 'elena.PNG',
+
+  },
+  {
+    character: 'Pluto',
+    characterImageArea: 'pluto.PNG',
+
+  },
+  {
+    character: 'Grumpy',
+    characterImageArea: 'grumpy.PNG',
+
+  },
+  {
+    character: 'Moana',
+    characterImageArea: 'moana.PNG',
+
+  },
+  {
+    character: 'Olaf',
+    characterImageArea: 'olaf.PNG',
+
+  },
+  {
+    character: 'Bo Peep',
+    characterImageArea: 'boPeep.PNG',
+
+  },
+  {
+    character: 'Lightening McQueen',
+    characterImageArea: 'lighteningMcQueen.PNG',
+
+  },
+  {
+    character: 'Simba',
+    characterImageArea: 'simba.PNG',
+
+  },
+  {
+    character: 'Yoda',
+    characterImageArea: 'yoda.PNG',
+
+  },
+  {
+    character: 'Ant Man',
+    characterImageArea: 'antMan.PNG',
+
+  },
+  {
+    character: 'Jack Skellington',
+    characterImageArea: 'jackSkellington.PNG',
+
+  },
+  {
+    character: 'Ariel',
+    characterImageArea: 'ariel.PNG',
+
+  },
+  {
+    character: 'Mike Wazowski',
+    characterImageArea: 'mikeWazowski.PNG',
+
+  },
+  {
+    character: 'Marie',
+    characterImageArea: 'marie.PNG',
+
+  },
+  {
+    character: 'Venom',
+    characterImageArea: 'venom.PNG',
+
+  },
+  {
+    character: 'Goofy',
+    characterImageArea: 'goofy.PNG',
+
+  },
+  {
+    character: 'Black Widow',
+    characterImageArea: 'blackWidow.PNG',
+
+  },
+  {
+    character: 'Alice',
+    characterImageArea: 'alice.PNG',
+
+  },
+  {
+    character: 'Mickey Mouse',
+    characterImageArea: 'mickeyMouse.PNG',
+
+  },
+  {
+    character: 'Hulk',
+    characterImageArea: 'hulk.PNG',
+
+  },
+  {
+    character: 'Mary Poppins',
+    characterImageArea: 'maryPoppins.PNG',
+
+  },
+  {
+    character: 'Woody',
+    characterImageArea: 'woody.PNG',
+
+  },
+  {
+    character: 'Tinker-bell',
+    characterImageArea: 'tinkerBell.PNG',
+
+  },
+  {
+    character: 'Captain Marvel',
+    characterImageArea: 'captainMarvel.PNG',
+
+  },
+  {
+    character: 'Anna',
+    characterImageArea: 'anna.PNG',
+
+  },
+  {
+    character: 'Daisy Duck',
+    characterImageArea: 'daisyDuck.PNG',
+
+  },
+  {
+    character: 'R2 D2',
+    characterImageArea: 'r2D2.PNG',
+
+  },
+  {
+    character: 'Sulley',
+    characterImageArea: 'sulley.PNG',
+
+  },
+  {
+    character: 'Forky',
+    characterImageArea: 'forky.PNG',
+
+  },
+  {
+    character: 'Mulan',
+    characterImageArea: 'mulan.PNG',
+
+  },
+  {
+    character: 'Wall-E',
+    characterImageArea: 'wallE.PNG',
+
+  },
+  {
+    character: 'Cheshire Cat',
+    characterImageArea: 'cheshireCat.PNG',
+
+  },
+  {
+    character: 'Darth Vader',
+    characterImageArea: 'darthVader.PNG',
+
+  },
+  {
+    character: 'Pinocchio',
+    characterImageArea: 'pinocchio.PNG',
+
+  },
+  {
+    character: 'Spider Man',
+    characterImageArea: 'spiderMan.PNG',
+
+  },
+  {
+    character: 'Hercules',
+    characterImageArea: 'hercules.PNG',
+
+  },
+  {
+    character: 'Rex',
+    characterImageArea: 'rex.PNG',
+
+  },
+  {
+    character: 'Cinderella',
+    characterImageArea: 'cinderella.PNG',
+
+  },
+  {
+    character: 'Han Solo',
+    characterImageArea: 'hanSolo.PNG',
+
+  },
+  {
+    character: 'Captain America',
+    characterImageArea: 'captainAmerica.PNG',
+
+  },
+  {
+    character: 'Winnie the Pooh',
+    characterImageArea: 'winnieThePooh.PNG',
+
+  },
+  {
+    character: 'Chip',
+    characterImageArea: 'chip.PNG',
+
+  },
+  {
+    character: 'Elsa',
+    characterImageArea: 'elsa.PNG',
+
+  },
+  {
+    character: 'Piglet',
+    characterImageArea: 'piglet.PNG',
+
+  },
+  {
+    character: 'Thanos',
+    characterImageArea: 'thanos.PNG',
+
+  },
+  {
+    character: 'Dumbo',
+    characterImageArea: 'dumbo.PNG',
+
+  },
+  {
+    character: 'Stitch',
+    characterImageArea: 'stitch.PNG',
+
+  },
+  {
+    character: 'Rapunzel',
+    characterImageArea: 'rapunzel.PNG',
+
+  },
+  {
+    character: 'Lotso',
+    characterImageArea: 'lotso.PNG',
+
+  },
+  {
+    character: 'Doc McStuffins',
+    characterImageArea: 'docMcStuffins.PNG',
+
+  },
+  {
+    character: 'BB-8',
+    characterImageArea: 'bb8.PNG',
+
+  },
+  {
+    character: 'Tigger',
+    characterImageArea: 'tigger.PNG',
+
+  },
+  {
+    character: 'Belle',
+    characterImageArea: 'belle.PNG',
+
+  },
+  {
+    character: 'Aladdin',
+    characterImageArea: 'aladdin.png',
+
+  },
+  {
+    character: 'Black Panther',
+    characterImageArea: 'blackPanther.PNG',
+
+  },
+  {
+    character: 'Vampirina',
+    characterImageArea: 'vampirina.PNG',
+
+  },
+  {
+    character: 'Bambi',
+    characterImageArea: 'bambi.PNG',
+
+  },
+  {
+    character: 'Kermit',
+    characterImageArea: 'kermit.PNG',
+
+  },
+  {
+    character: 'Baymax',
+    characterImageArea: 'baymax.PNG',
+
+  },
+  {
+    character: 'Stormtrooper',
+    characterImageArea: 'stormtrooper.PNG',
+
+  },
+  {
+    character: 'Aurora',
+    characterImageArea: 'aurora.PNG',
+
+  },
+  {
+    character: 'Peter Pan',
+    characterImageArea: 'peterPan.PNG',
+
+  },
+  {
+    character: 'Iron Man',
+    characterImageArea: 'ironMan.PNG',
+
+  },
+  {
+    character: 'Jasmine',
+    characterImageArea: 'jasmine.PNG',
+
+  },
+  {
+    character: 'Grogu',
+    characterImageArea: 'grogu.PNG',
+
+  },
+  {
+    character: 'Tiana',
+    characterImageArea: 'tiana.PNG',
+
+  },
+  {
+    character: 'Thor',
+    characterImageArea: 'thor.PNG',
+
+  },
+  {
+    character: 'Buzz Lightyear',
+    characterImageArea: 'buzzLightyear.PNG',
+
+  },
+  {
+    character: 'Beast',
+    characterImageArea: 'beast.PNG',
+
+  },
+  {
+    character: 'Snow White',
+    characterImageArea: 'snowWhite.PNG',
+
+  },
+  {
+    character: 'Donald Duck',
+    characterImageArea: 'donaldDuck.PNG',
+
+  },
+  {
+    character: 'Merida',
+    characterImageArea: 'merida.PNG',
+
+  },
+  {
+    character: 'Eeyore',
+    characterImageArea: 'eeyore.PNG',
+
+  },
+  {
+    character: 'Jessie',
+    characterImageArea: 'jessie.PNG',
+
+  },
+  {
+    character: 'Pocahontas',
+    characterImageArea: 'pocahontas.PNG',
+
+  },
+  {
+    character: 'Minnie Mouse',
+    characterImageArea: 'minnieMouse.PNG',
+
+  },
 
 ];
 
@@ -408,12 +408,12 @@ generateButton.addEventListener('click', generateCharacter);
  * // https://www.sitepoint.com/simple-javascript-quiz/ referenced for how to return characters to inner html.
  */
 function generateCharacter() {
-   for (let i = 0; i < characters.length; i++) {
+  for (let i = 0; i < characters.length; i++) {
 
-      let x = characters[selectedCharacter];
-      characterArea.innerHTML = `<h3> ${x.character} </h3>`;
-      imageCharacter.innerHTML = `<img src= ${characterImages}${x.characterImageArea} alt = ${x.character} >`;
-   }
+    let x = characters[selectedCharacter];
+    characterArea.innerHTML = `<h3> ${x.character} </h3>`;
+    imageCharacter.innerHTML = `<img src= ${characterImages}${x.characterImageArea} alt = ${x.character} >`;
+  }
 }
 
 //Event Listener added to previous button to return to previous character image. It calls the function prevItem.
@@ -426,11 +426,11 @@ prevButton.addEventListener('click', prevItem);
  */
 
 function prevItem() {
-   if (selectedCharacter == characters[0]) {
+  if (selectedCharacter == characters[0]) {
 
-   } else {
-      generateCharacter(characters[selectedCharacter--]);
-   }
+  } else {
+    generateCharacter(characters[selectedCharacter--]);
+  }
 }
 //Event Listener added to next button to go to next character image. It calls the function nextItem.
 nextButton.addEventListener('click', nextItem);
@@ -441,11 +441,11 @@ nextButton.addEventListener('click', nextItem);
  * Continue through the generateCharacter iteration of objects.
  */
 function nextItem() {
-   if (selectedCharacter == characters.length - 1) {
+  if (selectedCharacter == characters.length - 1) {
 
-   } else {
-      generateCharacter(characters[selectedCharacter++]);
-   }
+  } else {
+    generateCharacter(characters[selectedCharacter++]);
+  }
 }
 
 //Event Listener added to confirm button to run the function getCharacter
@@ -460,19 +460,19 @@ confirmButton.addEventListener('click', getCharacter);
 
 function getCharacter() {
 
-   let chosenCharacter = document.getElementById('character').innerHTML;
-   let chosenImage = document.getElementById('character-image-area').innerHTML;
+  let chosenCharacter = document.getElementById('character').innerHTML;
+  let chosenImage = document.getElementById('character-image-area').innerHTML;
 
-   localStorage.setItem('character', chosenCharacter);
-   localStorage.setItem('character-image-area', chosenImage);
-   loadCharacter();
+  localStorage.setItem('character', chosenCharacter);
+  localStorage.setItem('character-image-area', chosenImage);
+  loadCharacter();
 
 }
 
 function loadCharacter() {
-   let chosenCharacter = localStorage.getItem('character');
-   document.getElementById('character').value = chosenCharacter;
+  let chosenCharacter = localStorage.getItem('character');
+  document.getElementById('character').value = chosenCharacter;
 
-   let chosenImage = localStorage.getItem('character-image-area');
-   document.getElementById('character-image-area').value = chosenImage;
+  let chosenImage = localStorage.getItem('character-image-area');
+  document.getElementById('character-image-area').value = chosenImage;
 }
